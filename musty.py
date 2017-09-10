6dab6780444c193b5971eedcd3d73d32e17d0283
mustymustafa <mustafaumar70@live.com>
2017-09-10 11:33:31 +0100

    Add files via upload

---
+++ musty.py
@@ -0,0 +1,24 @@
+
+
+sum = 0.0
+numbers = 1.1,2,3,4,5,3,2,3
+b = len(numbers)
+
+
+print("The count of numbers entered is:")
+print b
+
+print("The list of numbers in are: ")
+for n in numbers: 
+
+    
+ 
+    print n
+    sum += float(n)
+print ("sum of the numbers is: ")
+print sum
+
+print("Calculating average.......")
+print("The average of the numbers is: ")
+average = (sum/b)
+print(average)
